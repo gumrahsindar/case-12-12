bddb217aa46a6410e2b5a20443516e8749e131ea
gumrahsindar <gsindar@gmail.com>
2024-12-12 14:04:13 +0300

    refactor: remove LoginForm component and associated login page

--- pages/login/index.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <main class="mt-44">
-    <div class="flex items-start justify-center h-screen">
-      <LoginForm />
-    </div>
-  </main>
-</template>
