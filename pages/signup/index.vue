cbb6feb2eec5616b26037ae9e9c89f65d408e9d6
gumrahsindar <gsindar@gmail.com>
2024-12-12 14:03:05 +0300

    feat: add Signup page with registration form and SEO metadata

---
+++ pages/signup/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+useHead({
+  title: 'Signup | Catify',
+  meta: [
+    {
+      name: 'description',
+      content: 'Signup to see beautiful cats!',
+    },
+  ],
+})
+</script>
+
+<template>
+  <main class="mt-24">
+    <div class="flex flex-col items-center gap-10">
+      <div class="text-center space-y-4">
+        <h1 class="mt-10 text-4xl font-semibold">Catify</h1>
+        <p>Please register to see beautiful cats!</p>
+      </div>
+      <SignupForm />
+    </div>
+  </main>
+</template>
