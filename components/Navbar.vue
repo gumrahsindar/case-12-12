14513167602e98c8ec229120fb344de7d29f61cc
gumrahsindar <gsindar@gmail.com>
2024-12-12 14:01:22 +0300

    feat: enhance Navbar with user authentication display and logout functionality

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAuthStore } from '~/store/authStore'
+
+const authStore = useAuthStore()
+</script>
 
 <template>
-  <div class="flex items-center justify-between">
+  <nav class="flex items-center justify-between">
     <Logo />
+    <small v-if="authStore.user.isLoggedIn" class="ml-auto mr-4"
+      >Logged in as: {{ authStore.user.email }}</small
+    >
+    <Button
+      v-if="authStore.user.isLoggedIn"
+      class="mr-4"
+      size="sm"
+      variant="destructive"
+      @click="authStore.logout"
+      >Logout</Button
+    >
     <ThemeSwitch />
-  </div>
+  </nav>
 </template>
 
 <style scoped></style>
